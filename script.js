ea7b6dd7e0b33c6f788cbf5398ed49d75784ae27
Lihuê Oli. <152817849+DAMULIHUE@users.noreply.github.com>
2025-05-05 22:31:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 const textoHeader = document.getElementById('portfolio');
 const fonts = ['serif', 'system-ui', 'Arial', 'monospace', 'cursive'];
 let i = 0;
-// function oi() {
-//     setTimeout(function(){oi()}, 3000);
-//     console.log('oi');
-// }
-// oi();
 
 function mudarFonte(){
 
     textoHeader.style.fontFamily = fonts[i];
     i = (i + 1) % fonts.length;
-    console.log(i);
 
     setTimeout(function(){mudarFonte()}, 2000);
 }
